9727d5efda6006a461e374ab281066dfe30690b7
syllable24 <syllable24@protonmail.com>
2023-11-30 21:04:17 +0100

    Added AnalysisResult struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+struct AnalysisResult {
+	six_count: u64,
+	less_than_two_count: u64,
+	even_count: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+	let v: Vec<u64> = vec![1,2,3];
+	
+    let _result = process_numbers(v);	
+}
+
+fn process_numbers(_numbers: Vec<u64>) -> AnalysisResult {
+	let result = AnalysisResult{
+		six_count: 0,
+		less_than_two_count: 0,
+		even_count: 0		
+	};
+	
+	return result;
 }
